929b59edad11307139d5059e296754536f6c3270
ColdSilent <51171699+ColdSilent@users.noreply.github.com>
2023-11-24 14:25:24 -0300

    Update CreateTable.sql

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS dbo.Usuario
 CREATE TABLE dbo.Usuario (
     idUsuario INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	idPersona INT FOREIGN KEY REFERENCES Persona(idPersona) NOT NULL,
-    NombreUsuario VARCHAR(50) NOT NULL,
-    Contrasenha VARCHAR(50) NOT NULL,
-	Nivel VARCHAR(50) NOT NULL,
-	Estado VARCHAR(50) NOT NULL
+    NombreUsuario VARCHAR(75) NOT NULL,
+    Contrasenha VARCHAR(75) NOT NULL,
+	Nivel VARCHAR(75) NOT NULL,
+	Estado VARCHAR(75) NOT NULL
 );
